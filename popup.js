cb0bfc935ce81b335c5bed4dbd46273aa4b19f28
Ricardo Rojas Rodríguez <37809835+Ricardo0697@users.noreply.github.com>
2025-02-24 12:42:47 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,27 +1,32 @@
 document.getElementById("increase-font").addEventListener("click", () => {
-    chrome.scripting.executeScript({
-      target: { allFrames: true },
-      func: () => {
-        document.body.style.fontSize = `${parseInt(window.getComputedStyle(document.body).fontSize) + 2}px`;
-      }
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: () => {
+          document.body.style.fontSize = `${parseInt(window.getComputedStyle(document.body).fontSize) + 2}px`;
+        }
+      });
     });
   });
   
   document.getElementById("decrease-font").addEventListener("click", () => {
-    chrome.scripting.executeScript({
-      target: { allFrames: true },
-      func: () => {
-        document.body.style.fontSize = `${parseInt(window.getComputedStyle(document.body).fontSize) - 2}px`;
-      }
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: () => {
+          document.body.style.fontSize = `${parseInt(window.getComputedStyle(document.body).fontSize) - 2}px`;
+        }
+      });
     });
   });
   
   document.getElementById("toggle-contrast").addEventListener("click", () => {
-    chrome.scripting.executeScript({
-      target: { allFrames: true },
-      func: () => {
-        document.body.style.filter = document.body.style.filter === "invert(1)" ? "none" : "invert(1)";
-      }
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: () => {
+          document.body.style.filter = document.body.style.filter === "invert(1)" ? "none" : "invert(1)";
+        }
+      });
     });
   });
-  
