8b681cc9c4d28936c72c5a02750b4a95a531d20c
Ricardo Rojas Rodríguez <37809835+Ricardo0697@users.noreply.github.com>
2025-02-24 12:35:30 -0600

    many fixes
    add icon.
    fix manifest.json
    Fix Popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,27 @@
 document.getElementById("increase-font").addEventListener("click", () => {
-    chrome.scripting.executeScript({ target: { allFrames: true }, func: () => changeFontSize('+') });
+    chrome.scripting.executeScript({
+      target: { allFrames: true },
+      func: () => {
+        document.body.style.fontSize = `${parseInt(window.getComputedStyle(document.body).fontSize) + 2}px`;
+      }
+    });
   });
   
   document.getElementById("decrease-font").addEventListener("click", () => {
-    chrome.scripting.executeScript({ target: { allFrames: true }, func: () => changeFontSize('-') });
+    chrome.scripting.executeScript({
+      target: { allFrames: true },
+      func: () => {
+        document.body.style.fontSize = `${parseInt(window.getComputedStyle(document.body).fontSize) - 2}px`;
+      }
+    });
   });
   
   document.getElementById("toggle-contrast").addEventListener("click", () => {
-    chrome.scripting.executeScript({ target: { allFrames: true }, func: () => toggleContrast() });
+    chrome.scripting.executeScript({
+      target: { allFrames: true },
+      func: () => {
+        document.body.style.filter = document.body.style.filter === "invert(1)" ? "none" : "invert(1)";
+      }
+    });
   });
+  
